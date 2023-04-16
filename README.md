c9cb655ec6bb05c8945ae4ee9dfea1bcf8486b3c
baks.dev <admin@baks.dev>
2023-04-16 20:48:26 +0300

    Baks Development

---
+++ README.md
@@ -0,0 +1,43 @@
+# BaksDev Product Stocks
+
+![Version](https://img.shields.io/badge/version-6.2.0-blue) ![php 8.1+](https://img.shields.io/badge/php-min%208.1-red.svg)
+
+Модуль складского учета продукции
+
+## Установка
+
+``` bash
+$ composer require baks-dev/products-stocks
+```
+
+## Дополнительно
+
+Установка файловых ресурсов в публичную директорию (javascript, css, image ...):
+
+``` bash
+$ php bin/console baks:assets:install
+```
+
+Роли администартора с помощью Fixtures
+
+``` bash
+$ php bin/console doctrine:fixtures:load --append
+```
+
+Изменения в схеме базы данных с помощью миграции
+
+``` bash
+$ php bin/console doctrine:migrations:diff
+
+$ php bin/console doctrine:migrations:migrate
+```
+
+## Журнал изменений ![Changelog](https://img.shields.io/badge/changelog-yellow)
+
+О том, что изменилось за последнее время, обратитесь к [CHANGELOG](CHANGELOG.md) за дополнительной информацией.
+
+## Лицензия ![License](https://img.shields.io/badge/MIT-green)
+
+The MIT License (MIT). Обратитесь к [Файлу лицензии](LICENSE.md) за дополнительной информацией.
+
+
---
+++ Resources/config/services.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+//use App\Module\Product\Type\Category\Id\CategoryUidConverter;
+//use BaksDev\Users\Entity\User;
+//use App\Module\Product\Entity;
+//use App\Module\Product\EntityListeners;
+
+return static function(ContainerConfigurator $configurator) {
+	$services = $configurator->services()
+		->defaults()
+		->autowire()
+		->autoconfigure()
+	;
+	
+	$namespace = 'BaksDev\Products\Stocks';
+	
+//	$services->load($namespace.'\Controller\\', __DIR__.'/../../Controller')
+//		->tag('controller.service_arguments')
+//	;
+//	
+//	$services->load($namespace.'\Repository\\', __DIR__.'/../../Repository')
+//		->exclude(__DIR__.'/../../Repository/**/*DTO.php')
+//	;
+//	//->tag('controller.service_arguments');
+//	
+//	$services->load($namespace.'\UseCase\\', __DIR__.'/../../UseCase')
+//		->exclude(__DIR__.'/../../UseCase/**/*DTO.php')
+//	;
+//	
+//	$services->load($namespace.'\DataFixtures\\', __DIR__.'/../../DataFixtures')
+//		->exclude(__DIR__.'/../../DataFixtures/**/*DTO.php')
+//	;
+//	
+//	$services->load($namespace.'\Forms\\', __DIR__.'/../../Forms');
+
+};
+
