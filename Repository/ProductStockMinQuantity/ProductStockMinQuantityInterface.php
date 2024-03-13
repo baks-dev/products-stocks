b779264ce10c66956a419d271a0395cd80cc26b1
kingstar <admin@baks.dev>
2024-03-13 18:12:09 +0300

    Baks Development

---
+++ Repository/ProductStockMinQuantity/ProductStockMinQuantityInterface.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace BaksDev\Products\Stocks\Repository\ProductStockMinQuantity;
+
+use BaksDev\Products\Product\Type\Id\ProductUid;
+use BaksDev\Products\Product\Type\Offers\ConstId\ProductOfferConst;
+use BaksDev\Products\Product\Type\Offers\Variation\ConstId\ProductVariationConst;
+use BaksDev\Products\Product\Type\Offers\Variation\Modification\ConstId\ProductModificationConst;
+use BaksDev\Products\Stocks\Entity\ProductStockTotal;
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
+
+interface ProductStockMinQuantityInterface
+{
+
+    public function profile(UserProfileUid $profile): self;
+
+    public function product(ProductUid $product): self;
+
+    public function offerConst(?ProductOfferConst $offer): self;
+
+    public function variationConst(?ProductVariationConst $variation): self;
+
+    public function modificationConst(?ProductModificationConst $modification): self;
+
+    /**
+     * Метод возвращает одно место складирования продукции с минимальным количеством в наличие без учета резерва
+     */
+    public function findOneBy(): ?ProductStockTotal;
+
+
+    /**
+     * Метод возвращает одно место складирования продукции с минимальным количеством в наличие c четом разности резерва
+     */
+    public function findOneBySubReserve(): ?ProductStockTotal;
+
+}
