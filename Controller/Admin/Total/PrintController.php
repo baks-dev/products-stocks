07e93f628e6c22d5b596c4b26e601b9bac0bdc8b
kingstar <admin@baks.dev>
2024-04-18 23:56:28 +0300

    Baks Development

---
+++ Controller/Admin/Total/PrintController.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace BaksDev\Products\Stocks\Controller\Admin\Total;
+
+use BaksDev\Core\Controller\AbstractController;
+use BaksDev\Core\Form\Search\SearchDTO;
+use BaksDev\Core\Form\Search\SearchForm;
+use BaksDev\Core\Listeners\Event\Security\RoleSecurity;
+use BaksDev\Core\Twig\CallTwigFuncExtension;
+use BaksDev\Products\Product\Forms\ProductFilter\Admin\ProductFilterDTO;
+use BaksDev\Products\Product\Forms\ProductFilter\Admin\ProductFilterForm;
+use BaksDev\Products\Stocks\Forms\WarehouseFilter\Admin\ProductsStocksFilterDTO;
+use BaksDev\Products\Stocks\Forms\WarehouseFilter\Admin\ProductsStocksFilterForm;
+use BaksDev\Products\Stocks\Repository\AllProductStocks\AllProductStocksInterface;
+use chillerlan\QRCode\QRCode;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+#[AsController]
+#[RoleSecurity('ROLE_PRODUCT_STOCK_INDEX')]
+final class PrintController extends AbstractController
+{
+    /**
+     * Печать остатков всего склада
+     */
+    #[Route('/admin/product/stock/print', name: 'admin.total.print', methods: ['GET', 'POST'])]
+    public function index(
+        Request $request,
+        AllProductStocksInterface $allProductStocks,
+        Environment $environment
+    ): Response
+    {
+        /**
+         * Фильтр продукции по ТП
+         */
+        $filter = new ProductFilterDTO($request);
+        $filter->allVisible();
+
+        $filterForm = $this->createForm(ProductFilterForm::class, $filter);
+        $filterForm->handleRequest($request);
+
+        $query = $allProductStocks
+            ->filter($filter)
+            ->setLimit(1000)
+            ->fetchAllProductStocksAssociative(
+                $this->getUsr()?->getId(),
+                $this->getProfileUid()
+            );
+
+        if(empty($query->getData()))
+        {
+            return new Response();
+        }
+
+        return $this->render([
+            'query' => $query->getData()
+        ]);
+
+    }
+}
