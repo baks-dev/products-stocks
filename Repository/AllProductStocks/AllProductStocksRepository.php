16742fc2e5561d64d6b3d748233b79bedd99adeb
kingstar <admin@baks.dev>
2024-07-28 19:40:07 +0300

    Baks Development

--- Repository/AllProductStocks/AllProductStocksRepository.php
+++ Repository/AllProductStocks/AllProductStocksRepository.php
@@ -523,9 +523,7 @@ final class AllProductStocksRepository implements AllProductStocksInterface
         $dbal->addOrderBy('product_offer.value');
         $dbal->addOrderBy('product_variation.value');
         $dbal->addOrderBy('product_variation.value');
-
-
-        //$dbal->addOrderBy('stock_product.total');
+        $dbal->addOrderBy('stock_product.total');
 
         if($this->limit)
         {
