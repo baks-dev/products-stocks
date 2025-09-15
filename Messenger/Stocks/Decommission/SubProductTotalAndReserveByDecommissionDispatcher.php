23d8fbda1679ac886176445ce15131a1708de494
MichelAngelo <baksdevelopmen@gmail.com>
2025-09-15 23:51:46 +0300

    BaksDevelopment

--- Messenger/Stocks/Decommission/SubProductTotalAndReserveByDecommissionDispatcher.php
+++ Messenger/Stocks/Decommission/SubProductTotalAndReserveByDecommissionDispatcher.php
@@ -148,14 +148,16 @@ final readonly class SubProductTotalAndReserveByDecommissionDispatcher
             }
 
             /** Списываем складской остаток */
-            $this->messageDispatch->dispatch(new SubProductStocksTotalAndReserveMessage(
-                $OrderEvent->getMain(),
-                $OrderEvent->getOrderProfile(),
-                $CurrentProductIdentifier->getProduct(),
-                $CurrentProductIdentifier->getOfferConst(),
-                $CurrentProductIdentifier->getVariationConst(),
-                $CurrentProductIdentifier->getModificationConst(),
-            )->setTotal($product->getPrice()->getTotal()));
+            $this->messageDispatch->dispatch(
+                new SubProductStocksTotalAndReserveMessage(
+                    $OrderEvent->getMain(),
+                    $OrderEvent->getOrderProfile(),
+                    $CurrentProductIdentifier->getProduct(),
+                    $CurrentProductIdentifier->getOfferConst(),
+                    $CurrentProductIdentifier->getVariationConst(),
+                    $CurrentProductIdentifier->getModificationConst(),
+                )
+                    ->setTotal($product->getPrice()->getTotal()));
 
             $this->logger->info(
                 sprintf(
