d3d3ccfed1bb7d9f4c5c9dd5f4fbc40eed325895
MichelAngelo <baksdevelopmen@gmail.com>
2025-10-15 16:21:40 +0300

    BaksDevelopment

---
+++ UseCase/Admin/Decommission/Products/NewDecommissionOrderProductForm.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Products\Stocks\UseCase\Admin\Decommission\Products;
+
+use BaksDev\Products\Product\Type\Event\ProductEventUid;
+use BaksDev\Products\Product\Type\Offers\Id\ProductOfferUid;
+use BaksDev\Products\Product\Type\Offers\Variation\Id\ProductVariationUid;
+use BaksDev\Products\Product\Type\Offers\Variation\Modification\Id\ProductModificationUid;
+use BaksDev\Products\Stocks\UseCase\Admin\Decommission\Products\Price\NewDecommissionOrderPriceForm;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class NewDecommissionOrderProductForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+        // Продукт
+
+        $builder->add('product', HiddenType::class);
+
+        $builder->get('product')->addModelTransformer(
+            new CallbackTransformer(
+                function($product) {
+                    return $product instanceof ProductEventUid ? $product->getValue() : $product;
+                },
+                function($product) {
+                    return new ProductEventUid($product);
+                },
+            ),
+        );
+
+        // Торговое предложение
+
+        $builder->add('offer', HiddenType::class);
+
+        $builder->get('offer')->addModelTransformer(
+            new CallbackTransformer(
+                function($offer) {
+                    return $offer instanceof ProductOfferUid ? $offer->getValue() : $offer;
+                },
+                function($offer) {
+                    return $offer ? new ProductOfferUid($offer) : null;
+                },
+            ),
+        );
+
+        // Множественный вариант
+
+        $builder->add('variation', HiddenType::class);
+
+        $builder->get('variation')->addModelTransformer(
+            new CallbackTransformer(
+                function($variation) {
+                    return $variation instanceof ProductVariationUid ? $variation->getValue() : $variation;
+                },
+                function($variation) {
+                    return $variation ? new ProductVariationUid($variation) : null;
+                },
+            ),
+        );
+
+        // Модификация множественного варианта
+
+        $builder->add('modification', HiddenType::class);
+
+        $builder->get('modification')->addModelTransformer(
+            new CallbackTransformer(
+                function($modification) {
+                    return $modification instanceof ProductModificationUid ? $modification->getValue() : $modification;
+                },
+                function($modification) {
+
+                    return $modification ? new ProductModificationUid($modification) : null;
+                },
+            ),
+        );
+
+        $builder->add('price', NewDecommissionOrderPriceForm::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => NewDecommissionOrderProductDTO::class,
+        ]);
+    }
+}
