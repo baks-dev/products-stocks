54ef519bdfbf31356b96ee614283f4ebcee2f635
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-20 22:57:23 +0300

    BaksDevelopment

--- Commands/ProductCardRecalculateCommand.php
+++ Commands/ProductCardRecalculateCommand.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -43,7 +43,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'baks:product:card:recalculate',
-    description: 'Перерасчет остатка карточки согласно складским остаткам'
+    description: 'Перерасчет остатка всех карточек согласно складским остаткам'
 )]
 class ProductCardRecalculateCommand extends Command
 {
