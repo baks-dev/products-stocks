c2e6bab21f6d64ea736f02cdbb0272e6e1582b5e
MichelAngelo <baksdevelopmen@gmail.com>
2026-04-04 00:47:04 +0300

    BaksDevelopment

---
+++ Messenger/Warmup/ProductCatalog/ProductCatalogWarmupDispatcher.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Products\Stocks\Messenger\Warmup\ProductCatalog;
+
+
+use BaksDev\Core\Deduplicator\DeduplicatorInterface;
+use BaksDev\Products\Product\Repository\Cards\ProductCatalog\ProductCatalogInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[Autoconfigure(shared: false)]
+#[AsMessageHandler(priority: 0)]
+final class ProductCatalogWarmupDispatcher
+{
+    public function __construct(
+        private DeduplicatorInterface $deduplicator,
+        private ProductCatalogInterface $ProductCatalogRepository
+    ) {}
+
+    public function __invoke(ProductCatalogWarmupMessage $message): void
+    {
+        $Deduplicator = $this->deduplicator
+            ->namespace('warmup')
+            ->expiresAfter('1 minute')
+            ->deduplication([self::class]);
+
+        if($Deduplicator->isExecuted())
+        {
+            return;
+        }
+
+        $Deduplicator->save();
+
+        $this->ProductCatalogRepository
+            ->forCategory($message->getCategory())
+            ->onlyActive()
+            ->maxResult(6)
+            ->findAll();
+
+        $Deduplicator->delete();
+    }
+}
