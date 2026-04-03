e1120c49a61f4bd03c00794bfbb8d818849b3097
adminap <shestopalov92@gmail.com>
2026-04-03 15:27:04 +0300

    fork: PR-03.04.2026. Отмена складской завки со статусом ProductStockStatusError

--- UseCase/Admin/Package/PackageProductStockHandler.php
+++ UseCase/Admin/Package/PackageProductStockHandler.php
@@ -19,6 +19,7 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
+ *
  */
 
 declare(strict_types=1);
@@ -54,7 +55,7 @@ final class PackageProductStockHandler extends AbstractHandler
                 $this->main->getEvent(),
                 $command->getEvent(),
             ),
-            transport: 'orders-order',
+            transport: 'products-stocks',
         );
 
         return $this->main;
