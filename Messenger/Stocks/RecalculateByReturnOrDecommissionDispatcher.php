f94aa83658e4040e49dff3b99727ba302d9d2a93
MichelAngelo <baksdevelopmen@gmail.com>
2026-04-29 12:56:20 +0300

    BaksDevelopment

---
+++ Messenger/Stocks/RecalculateByReturnOrDecommissionDispatcher.php
@@ -0,0 +1,160 @@
+<?php
+/*
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Products\Stocks\Messenger\Stocks;
+
+use BaksDev\Core\Deduplicator\DeduplicatorInterface;
+use BaksDev\Core\Messenger\MessageDispatchInterface;
+use BaksDev\Orders\Order\Entity\Event\OrderEvent;
+use BaksDev\Orders\Order\Messenger\OrderMessage;
+use BaksDev\Orders\Order\Repository\CurrentOrderEvent\CurrentOrderEventInterface;
+use BaksDev\Orders\Order\Repository\OrderEvent\OrderEventInterface;
+use BaksDev\Orders\Order\Type\Status\OrderStatus\Collection\OrderStatusDecommission;
+use BaksDev\Orders\Order\Type\Status\OrderStatus\Collection\OrderStatusReturn;
+use BaksDev\Orders\Order\UseCase\Admin\Edit\EditOrderDTO;
+use BaksDev\Orders\Order\UseCase\Admin\Edit\Products\OrderProductDTO;
+use BaksDev\Products\Product\Repository\CurrentProductIdentifier\CurrentProductIdentifierByEventInterface;
+use BaksDev\Products\Product\Repository\CurrentProductIdentifier\CurrentProductIdentifierResult;
+use BaksDev\Products\Stocks\Messenger\Products\Recalculate\RecalculateProductMessage;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+/**
+ *  Пересчет остатков продукции в карточке товара, если создается заказ со статусом Decommission «Списание»
+ */
+#[Autoconfigure(shared: false)]
+#[AsMessageHandler(priority: 900)]
+final readonly class RecalculateByReturnOrDecommissionDispatcher
+{
+    public function __construct(
+        #[Target('ordersOrderLogger')] private LoggerInterface $logger,
+        private DeduplicatorInterface $deduplicator,
+        private OrderEventInterface $OrderEventRepository,
+        private CurrentProductIdentifierByEventInterface $CurrentProductIdentifierRepository,
+        private MessageDispatchInterface $messageDispatch,
+    ) {}
+
+    public function __invoke(OrderMessage $message): void
+    {
+        $Deduplicator = $this->deduplicator
+            ->namespace('orders-order')
+            ->deduplication([
+                (string) $message->getId(),
+                self::class,
+            ]);
+
+        if($Deduplicator->isExecuted())
+        {
+            return;
+        }
+
+        $OrderEvent = $this->OrderEventRepository->find($message->getEvent());
+
+        if(false === ($OrderEvent instanceof OrderEvent))
+        {
+            $this->logger->critical(
+                'products-sign: Не найдено событие OrderEvent',
+                [self::class.':'.__LINE__, var_export($message, true)],
+            );
+
+            return;
+        }
+
+        /**
+         * Завершаем обработчик, если статус заказа не является
+         * - Decommission «Списание»
+         * - Return «Возврат»
+         */
+        if(
+            false === $OrderEvent->isStatusEquals(OrderStatusDecommission::class)
+            && false === $OrderEvent->isStatusEquals(OrderStatusReturn::class)
+        )
+        {
+            $Deduplicator->save();
+            return;
+        }
+
+        $EditOrderDTO = new EditOrderDTO();
+        $OrderEvent->getDto($EditOrderDTO);
+
+        /** @var OrderProductDTO $product */
+        foreach($EditOrderDTO->getProduct() as $product)
+        {
+            /** Получаем активные идентификаторы карточки для преобразования в константы */
+            $CurrentProductIdentifier = $this->CurrentProductIdentifierRepository
+                ->forEvent($product->getProduct())
+                ->forOffer($product->getOffer())
+                ->forVariation($product->getVariation())
+                ->forModification($product->getModification())
+                ->find();
+
+            if(false === ($CurrentProductIdentifier instanceof CurrentProductIdentifierResult))
+            {
+                $this->logger->critical(
+                    'products-sign: Продукт для пересчета складских остатков не найден',
+                    [
+                        'product' => (string) $product->getProduct(),
+                        'offer' => (string) $product->getOffer(),
+                        'variation' => (string) $product->getVariation(),
+                        'modification' => (string) $product->getModification(),
+                        self::class.':'.__LINE__,
+                    ],
+                );
+
+                continue;
+            }
+
+            /**
+             * Пересчитываем остатки в карточке товара
+             */
+
+            $this->logger->info('products-sign: Пересчитываем остатки в карточке товара при списании на складе',
+                [
+                    'product' => (string) $product->getProduct(),
+                    'offer' => (string) $product->getOffer(),
+                    'variation' => (string) $product->getVariation(),
+                    'modification' => (string) $product->getModification(),
+                    self::class.':'.__LINE__,
+                ],
+            );
+
+            $RecalculateProductMessage = new RecalculateProductMessage(
+                product: $CurrentProductIdentifier->getProduct(),
+                offer: $CurrentProductIdentifier->getOfferConst(),
+                variation: $CurrentProductIdentifier->getVariationConst(),
+                modification: $CurrentProductIdentifier->getModificationConst(),
+            );
+
+            $this->messageDispatch->dispatch(
+                message: $RecalculateProductMessage,
+                transport: 'products-stocks-low',
+            );
+        }
+
+        $Deduplicator->save();
+    }
+}
